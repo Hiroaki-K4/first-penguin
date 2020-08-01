9d345af75da12a001044b216b77184523a99fe55
Hiroaki-K4 <49no19@gmail.com>
2020-08-01 13:29:55 +0900

    first

---
+++ layouts/default.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+      color="blue darken-1"
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+          class="white--text"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="blue darken-2"
+    >
+      <v-app-bar-nav-icon color="white" @click.stop="drawer = !drawer" />
+      <v-toolbar-title class="white--text" v-text="title" />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :absolute="!fixed"
+      app
+      color="grey darken-4"
+    >
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'All papers',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Computer vision',
+          to: '/vision'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'First penguin'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-flex
+      xs12
+      sm12
+      md12
+    >
+      <v-col
+          v-for="(item, i) in items"
+          :key="i"
+          cols="12"
+        >
+          <v-card
+            white
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="headline"
+                  v-text="item.title"
+                ></v-card-title>
+
+                <v-card-subtitle v-text="item.summary"></v-card-subtitle>
+                <v-card-subtitle v-text="item.summary_ja"></v-card-subtitle>
+                <v-btn :href=item.pdf target="_blank">PDF</v-btn>
+              </div>
+            </div>
+          </v-card>
+        </v-col>
+    </v-flex>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ''
+    }
+  },
+  async asyncData({ $axios }) {
+    const url = "https://first-penguin-284413.uc.r.appspot.com/penguin";
+    const response = await $axios.$get(url);
+    return {
+      items: response
+    };
+  }
+};
+</script>
